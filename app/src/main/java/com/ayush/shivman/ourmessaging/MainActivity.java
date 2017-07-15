42c7e7775b31bead49c55f43cacddbc90974efad
shivman7 <shivanshujaiswal15@gmail.com>
2017-07-15 18:03:26 +0530

    I changed this

--- app/src/main/java/com/ayush/shivman/ourmessaging/MainActivity.java
+++ app/src/main/java/com/ayush/shivman/ourmessaging/MainActivity.java
@@ -16,7 +16,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-
+//massa bc
 public class MainActivity extends AppCompatActivity {
     private static final String UNAME="USER";
     private static final String SENDER="SENDER";
